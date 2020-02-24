03d311a41b03731c1f43d4d0d2f93ad3cd2288d4
Geo1088 <georgej1088@gmail.com>
2020-02-24 17:59:31 -0500

    Add test cases for expandPermalink

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const test = require('ava');
 
-const {squashPermalink} = require('.');
+const {squashPermalink, expandPermalink} = require('.');
 
 test('squashPermalink', t => {
 	for (const [arg, expected] of Object.entries({
@@ -47,5 +47,17 @@ test('squashPermalink', t => {
 		'https://old.reddit.com/message/messages/123abc/?trailing#garbage': 'm,123abc',
 	})) {
 		t.is(squashPermalink(arg), expected);
+	}
+});
+
+test('expandPermalink', t => {
+	for (const [arg, expected] of Object.entries({
+		'l,123abc': 'https://www.reddit.com/comments/123abc',
+		'l,123abc,456def': 'https://www.reddit.com/comments/123abc/_/456def',
+		'm,123abc': 'https://www.reddit.com/message/messages/123abc',
+		'https://www.reddit.com/r/subreddit/comments/123abc/': 'https://www.reddit.com/r/subreddit/comments/123abc/',
+		'literally anything else': 'literally anything else',
+	})) {
+		t.is(expandPermalink(arg), expected);
 	}
 });
