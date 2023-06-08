55fdfec192decea86aca2373f2987a2c60690bea
Erin <github@eritbh.me>
2023-06-07 21:49:27 -0400

    oh my fucking god i forgot to export the class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './helpers/usernotes';
 export * from './types/rawUsernotes';
 export * from './classes/UsernotesData';
+export * from './classes/ToolboxClient';
 export * from './subConfig';
