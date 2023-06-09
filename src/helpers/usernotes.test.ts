ce8cb67bfd9991d03d69ac81c515203bb4899430
Erin <github@eritbh.me>
2023-06-09 15:00:20 -0400

    Add some more test cases and clarifications

--- src/helpers/usernotes.test.ts
+++ src/helpers/usernotes.test.ts
@@ -8,7 +8,7 @@ import {
 
 test('squashPermalink', t => {
 	for (const [arg, expected] of Object.entries({
-		// Comment links
+		// Comment links -> l,POSTID,COMMENTID
 		'https://www.reddit.com/r/subreddit/comments/123abc/some_link_slug/456def': 'l,123abc,456def',
 		'https://www.reddit.com/r/subreddit/comments/123abc/some_link_slug/456def/?trailing#garbage': 'l,123abc,456def',
 		'https://www.reddit.com/comments/123abc/some_link_slug/456def': 'l,123abc,456def',
@@ -21,7 +21,7 @@ test('squashPermalink', t => {
 		'https://old.reddit.com/r/subreddit/comments/123abc/some_link_slug/456def/?trailing#garbage': 'l,123abc,456def',
 		'https://old.reddit.com/comments/123abc/some_link_slug/456def': 'l,123abc,456def',
 		'https://old.reddit.com/comments/123abc/some_link_slug/456def/?trailing#garbage': 'l,123abc,456def',
-		// Submission links
+		// Submission links -> l,POSTID
 		'https://www.reddit.com/r/subreddit/comments/123abc': 'l,123abc',
 		'https://www.reddit.com/r/subreddit/comments/123abc/some_link_slug/?trailing#garbage': 'l,123abc',
 		'https://www.reddit.com/comments/123abc': 'l,123abc',
@@ -36,7 +36,7 @@ test('squashPermalink', t => {
 		'https://old.reddit.com/comments/123abc/some_link_slug/?trailing#garbage': 'l,123abc',
 		'https://redd.it/123abc': 'l,123abc',
 		'https://redd.it/123abc/?trailing#garbage': 'l,123abc',
-		// Old modmail links
+		// Old modmail links -> m,MESSAGEID
 		'https://www.reddit.com/r/subreddit/message/messages/123abc': 'm,123abc',
 		'https://www.reddit.com/r/subreddit/message/messages/123abc/?trailing#garbage': 'm,123abc',
 		'https://www.reddit.com/message/messages/123abc': 'm,123abc',
@@ -49,6 +49,9 @@ test('squashPermalink', t => {
 		'https://old.reddit.com/r/subreddit/message/messages/123abc/?trailing#garbage': 'm,123abc',
 		'https://old.reddit.com/message/messages/123abc': 'm,123abc',
 		'https://old.reddit.com/message/messages/123abc/?trailing#garbage': 'm,123abc',
+		// Everything else is passed through as-is, including new modmail
+		'https://mod.reddit.com/mail/all/123abc': 'https://mod.reddit.com/mail/all/123abc',
+		'literally anything else': 'literally anything else',
 	})) {
 		t.is(squashPermalink(arg), expected);
 	}
@@ -56,10 +59,15 @@ test('squashPermalink', t => {
 
 test('expandPermalink', t => {
 	for (const [arg, expected] of Object.entries({
+		// Squashed submission links
 		'l,123abc': 'https://www.reddit.com/comments/123abc',
+		// Squashed comment links
 		'l,123abc,456def': 'https://www.reddit.com/comments/123abc/_/456def',
+		// Squashed old modmail links
 		'm,123abc': 'https://www.reddit.com/message/messages/123abc',
+		// Everything else is passed through as-is
 		'https://www.reddit.com/r/subreddit/comments/123abc/': 'https://www.reddit.com/r/subreddit/comments/123abc/',
+		'https://mod.reddit.com/mail/all/123abc': 'https://mod.reddit.com/mail/all/123abc',
 		'literally anything else': 'literally anything else',
 	})) {
 		t.is(expandPermalink(arg), expected);
