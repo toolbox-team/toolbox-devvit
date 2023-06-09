9e982c63ab2202ef33c17f7f2e02a9a0b8b12440
Erin <github@eritbh.me>
2023-06-09 05:10:03 -0400

    docs???

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './helpers/usernotes';
 export * from './types/RawUsernotes';
+export * from './types/Usernote';
 export * from './classes/Usernotes';
 export * from './classes/ToolboxClient';
 // export * from './subConfig';
